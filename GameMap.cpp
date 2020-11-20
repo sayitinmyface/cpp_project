93218960bb6ef60b7ef597b60d0264db0fef6671
sayitinmyface <leo99roo@gmail.com>
2020-11-20 14:05:46 +0900

    11201405

--- GameMap.cpp
+++ GameMap.cpp
@@ -33,7 +33,7 @@ void settingGameMap()
     timeout(30);
     // int row = LINES-1,col = COLS-COLS,move_row = 0,move_col=0;
     int bt_row = LINES-1 , left_col = COLS/3 , right_col = COLS-COLS/3 , center_col = COLS/2; 
-    int row,col;
+    int row = bt_row-1,col = center_col+5;
     // 
     setOutLine();//테두리
     // setBlock(bt_row-1,center_col);//블럭 쌓기
@@ -58,20 +58,24 @@ void settingGameMap()
         if(switch_col==-5){switch_col = 5;}
         else{switch_col=-5;}
         random = rand()%8;
-        // mvaddch(row/2,COLS/2,PLAYER);
+        mvaddstr(row,col,"@(YH)");
         ch = getch();
         switch (ch)
         {
-            // case 'w':       
             case KEY_UP:    
                 if(is_move_okay(row-1,col)) row -= 1; break;
             case KEY_DOWN:  
                 if(is_move_okay(row+1,col)) row += 1; break;
             case KEY_LEFT:  
-                if(is_move_okay(row,col-1)) col -= 1; break;
+                if(is_move_okay(row,col-1)) row -= 1 , col += 5; break;
             case KEY_RIGHT: 
-                if(is_move_okay(row,col+1)) col += 1; break;
-                // mvaddch(row,col-1,E_TRACE);
+                if(is_move_okay(row,col+1))
+                {
+                    row -= 1 , col -= 5; 
+                    // mvaddch(row,col-1,E_TRACE);
+                    break;
+                } 
+                
         }
     }
     endwin();//모든 자원 반납
